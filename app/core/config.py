d70bd2658599f8c7d308b49549ba5636f0f68512
sofiya <118591968+sofiya05@users.noreply.github.com>
2024-09-14 13:44:45 +0300

    first commit

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     title: str = 'Приложение QRKot'
     description: str = 'Описание приложения QRKot'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'secret'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
